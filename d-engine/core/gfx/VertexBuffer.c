0cad1b3dc9fd2687902ca34ab2c538d8de8c3306
Zenito Pires <zenitopires@gmail.com>
2025-10-20 15:36:37 -0400

    feat: implement remainder vbo functions

--- d-engine/core/gfx/VertexBuffer.c
+++ d-engine/core/gfx/VertexBuffer.c
@@ -1,5 +1,6 @@
 #include "VertexBuffer.h"
 
+// VertexBufferCreate creates a vertex buffer object with given data and size.
 VertexBuffer* VertexBufferCreate(float* data, uint32_t size) {
     VertexBuffer* vb = malloc(sizeof(VertexBuffer));
     if (!vb) {
@@ -12,6 +13,7 @@ VertexBuffer* VertexBufferCreate(float* data, uint32_t size) {
     return vb;
 }
 
+// VertexBufferDelete deletes a vertex buffer object.
 void VertexBufferDelete(VertexBuffer* vb) {
     printf("Deleting vertex buffer, id: %d.\n", vb->rendererID);
     if (vb) {
@@ -19,4 +21,14 @@ void VertexBufferDelete(VertexBuffer* vb) {
         free(vb);
         printf("Deleted vertex buffer!\n");
     }
+}
+
+// VertexBuffer_Bind binds a vertex buffer object. The buffer object name is stored in vb->rendererID.
+void VertexBuffer_Bind(VertexBuffer* vb) {
+    glBindBuffer(GL_ARRAY_BUFFER, vb->rendererID);
+}
+
+// VertexBuffer_Unbind unbinds a previously bounded vertex buffer object.
+void VertexBuffer_Unbind(VertexBuffer* vb) {
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
--- d-engine/core/gfx/VertexBuffer.h
+++ d-engine/core/gfx/VertexBuffer.h
@@ -11,7 +11,9 @@ typedef struct VertexBuffer {
     uint32_t rendererID;
 } VertexBuffer;
 
-VertexBuffer* VertexBufferCreate(float* data, uint32_t size);
-void VertexBufferDelete(VertexBuffer* vb);
+VertexBuffer* VertexBuffer_Create(float* data, uint32_t size);
+void VertexBuffer_Delete(VertexBuffer* vb);
+void VertexBuffer_Bind(VertexBuffer* vb);
+void VertexBuffer_Unbind(VertexBuffer* vb);
 
 #endif
